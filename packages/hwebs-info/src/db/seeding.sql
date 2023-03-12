47c5796f5af60d5c5b181d364934d6eb899ed002
Henry Webster <hwebs@hwebs.info>
2023-03-11 23:40:05 -0500

    begin transitioning components

--- packages/hwebs-info/src/db/seeding.sql
+++ packages/hwebs-info/src/db/seeding.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS items;
 CREATE TABLE IF NOT EXISTS items (title TEXT NOT NULL);
-INSERT INTO items (title) VALUES ('post 1');
-INSERT INTO items (title) VALUES ('post 2');
-INSERT INTO items (title) VALUES ('post 3');
-INSERT INTO items (title) VALUES ('post 4');
-INSERT INTO items (title) VALUES ('post 5');
+INSERT INTO items (title) VALUES ('Code');
+INSERT INTO items (title) VALUES ('Games');
+INSERT INTO items (title) VALUES ('Music');
+INSERT INTO items (title) VALUES ('Animation');
+INSERT INTO items (title) VALUES ('About');
