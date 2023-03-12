7ae5b7d860a7062c7c91f53e490d632c4b22cada
Henry Webster <hwebs@hwebs.info>
2023-03-11 22:41:07 -0500

    run dynamodb as part of local tests

---
+++ packages/content-client/scripts/dynamodb/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DOCKER_FILE=$1
+CONTAINER_NAME="dynamodb-local"
+
+# TODO detect if container exists but is not running
+
+if [ $( docker ps -a | grep $CONTAINER_NAME | wc -l ) -gt 0 ]; then
+	docker container rm -f $CONTAINER_NAME
+fi
+
+docker-compose -f $DOCKER_FILE up -d
+
