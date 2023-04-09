1c63496cf5b4c49d1103dd51725851d9015c83b6
Henry Webster <hwebs@hwebs.info>
2023-04-09 13:59:55 -0400

    [hwebs-info] add dynamodb table to terraform

--- packages/tf/src/main.tf
+++ packages/tf/src/main.tf
@@ -8,5 +8,39 @@ terraform {
 }
 
 provider "aws" {
-	region = "us-east-1"
+	region = "us-east-2"
+}
+
+resource "aws_dynamodb_table" "hwebs-info-table" {
+
+	name = "hwebs-info-table"
+	billing_mode = "PROVISIONED"
+	read_capacity = 5
+	write_capacity = 5
+	hash_key = "id"
+	range_key = "type"
+	
+	attribute {
+		name = "id"
+		type = "S"
+	}
+
+	attribute {
+		name = "type"
+		type = "S"
+	}
+
+	attribute {
+		name = "category"
+		type = "S"
+	}
+
+	global_secondary_index {
+		name = "post-index"
+		hash_key = "category"
+		range_key = "id"
+		projection_type = "ALL"
+		read_capacity = 5
+		write_capacity = 5
+	}
 }
